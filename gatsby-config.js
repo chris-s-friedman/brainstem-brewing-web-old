95d7927009b25f21f5a7ebbfae5e8924e49d72bb
Chris Friedman <chris.s.friedman@gmail.com>
2020-02-09 21:36:33 -0500

    :chart_with_upwards_trend: add GA tracking code

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-111428441-3 `,
       },
     },
     {
